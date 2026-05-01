443b624e36e6b31c73600452c69e7b72e95c88dc
Samuel Cormier-Iijima <samuel@cormier-iijima.com>
2026-05-01 09:09:30 -0400

    fix(nixos): migrate resolved options to settings.Resolve

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,12 +76,14 @@
 
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    fallbackDns = [
-      "1.1.1.1#one.one.one.one"
-      "1.0.0.1#one.one.one.one"
-    ];
-    dnsovertls = "true";
+    settings.Resolve = {
+      DNSSEC = "true";
+      DNSOverTLS = "true";
+      FallbackDNS = [
+        "1.1.1.1#one.one.one.one"
+        "1.0.0.1#one.one.one.one"
+      ];
+    };
   };
 
   services.k3s = {
