efd0bd0a59d78865ee3c7cdd467367518533114a
Samuel Cormier-Iijima <samuel@cormier-iijima.com>
2026-05-22 13:18:34 -0400

    experimental features

--- sci/configuration.nix
+++ sci/configuration.nix
@@ -11,6 +11,11 @@
     ../nixos/nix-cache.nix
   ];
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
