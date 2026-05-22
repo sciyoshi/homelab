e91f48edca9e3ba1d1ce8fdd3f4eebee052a7d8c
Samuel Cormier-Iijima <samuel@cormier-iijima.com>
2026-05-22 12:40:01 -0400

    fix(nix): enable shared binary caches

---
+++ nixos/nix-cache.nix
@@ -0,0 +1,18 @@
+{
+  nix.settings = {
+    builders-use-substitutes = true;
+    trusted-users = [
+      "sciyoshi"
+    ];
+    extra-substituters = [
+      "https://cache.flox.dev"
+      "https://cache.numtide.com"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
